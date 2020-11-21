710490ada664c0d7801340933d4b3a050c2cc090
kunxin-chor <chorkunxin@yahoo.com>
2020-11-21 07:19:26 +0000

    Added add review for specific book

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Review
 from .forms import ReviewForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from books.models import Book
 # Create your views here.
 
 
@@ -14,7 +15,8 @@ def index(request):
 
 
 @login_required
-def create_review(request):
+def create_review(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
     if request.method == "POST":
         # fill in the form with what the user has typed in
         form = ReviewForm(request.POST)
@@ -24,11 +26,13 @@ def create_review(request):
             # when we specify Commit=False, means don't save to database directly
             review = form.save(commit=False)
             review.user = request.user  # request.user will contain the currently logged in user
+            review.book = book
             review.save()
             messages.success(request, "New review added!")
             return redirect(index)
     else:
         form = ReviewForm()
         return render(request, 'reviews/create_review.template.html', {
-            'form': form
+            'form': form,
+            'book': book
         })
