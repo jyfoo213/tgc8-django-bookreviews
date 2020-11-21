a69c63ea89d1ff4a8100d961aa232a68b4a3de51
kunxin-chor <chorkunxin@yahoo.com>
2020-11-21 07:41:33 +0000

    Broken the urls into multiple files

--- BookReviewsProject/urls.py
+++ BookReviewsProject/urls.py
@@ -16,29 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import books.views
-import reviews.views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('books/', books.views.index),
-    path('book/details/<book_id>', books.views.view_book_details, name="book_details_route"),
-    path('book/create', books.views.create_book),
-    path('book/edit/<book_id>', books.views.edit_book,
-         name='update_book_route'),
-    path('book/delete/<book_id>', books.views.delete_book,
-         name="delete_book_route"),
-    path('reviews/', reviews.views.index),
-    path('reviews/create/<book_id>', reviews.views.create_review, name="create_review_route"),
-    path('authors/', books.views.view_authors),
-    path('author/create', books.views.create_author),
-    path('author/delete/<author_id>', books.views.delete_author,
-         name="delete_author_route"),
-    path('author/edit/<author_id>', books.views.edit_author,
-         name="update_author_route"),
-    path('publishers', books.views.view_publishers),
-    path('publisher/create', books.views.create_publisher),
-    path('publisher/delete/<publisher_id>', books.views.delete_publisher,
-         name="delete_publisher_route")
+    path('books/', include('books.urls')),
+    path('reviews/', include('reviews.urls'))
 ]
---
+++ books/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+import books.views
+
+urlpatterns = [
+    path('', books.views.index),
+    path('book/details/<book_id>', books.views.view_book_details, name="book_details_route"),
+    path('book/create', books.views.create_book),
+    path('book/edit/<book_id>', books.views.edit_book,
+         name='update_book_route'),
+    path('book/delete/<book_id>', books.views.delete_book,
+         name="delete_book_route"),
+    path('authors/', books.views.view_authors),
+    path('author/create', books.views.create_author),
+    path('author/delete/<author_id>', books.views.delete_author,
+         name="delete_author_route"),
+    path('author/edit/<author_id>', books.views.edit_author,
+         name="update_author_route"),
+    path('publishers', books.views.view_publishers),
+    path('publisher/create', books.views.create_publisher),
+    path('publisher/delete/<publisher_id>', books.views.delete_publisher,
+         name="delete_publisher_route")
+]
